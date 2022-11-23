c762f54b8e5c67a277b070d546639ca61232c675
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-23 18:33:38 +0900

    asd
    asd

--- src/components/MainConts.jsx
+++ src/components/MainConts.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
-import sblc from "../assets/img/weather/Sun behind large cloud.png";
-import sbc from "../assets/img/weather/Sun behind cloud.png";
-import dog from "../assets/img/animal/Dog.png";
-import cat from "../assets/img/animal/Black cat.png";
+import Today from "./Today";
 
 // xml을 json으로 변환해주는 xmlToJson함수 선언
 function xmlToJson(xml) {
   // Create the return object
   var obj = {};
 
-  if (xml.nodeType == 1) {
+  if (xml.nodeType === 1) {
     // element
     // do attributes
     if (xml.attributes.length > 0) {
@@ -21,7 +17,7 @@ function xmlToJson(xml) {
         obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
       }
     }
-  } else if (xml.nodeType == 3) {
+  } else if (xml.nodeType === 3) {
     // text
     obj = xml.nodeValue;
   }
@@ -83,138 +79,9 @@ const MainConts = () => {
   const [on, setOn] = useState([]);
 
   useEffect(() => {
-    getXMLfromAPI()
-      //.then((data) => console.log(data))
-      .then((data) => setOn(data));
+    getXMLfromAPI().then((data) => setOn(data));
   }, []);
-  return (
-    <main id="main">
-      <div class="wrap gmark">
-        <div class="left">
-          <div class="title">
-            <h1>OWATD</h1>
-            <p>
-              바쁜 일상 속 당신의 하루를
-              <br />
-              미리 준비해드립니다 :
-            </p>
-          </div>
-          <div class="week card">
-            <ul>
-              <li>
-                <h3>TODAY</h3>
-                <div class="icon">
-                  <img src={sblc} alt="todayicon" />
-                </div>
-                <div class="temp">
-                  <span class="high">5</span>
-                  <span class="low">12</span>
-                </div>
-              </li>
-              <li>
-                <h3>THU</h3>
-                <div class="icon">
-                  <img
-                    src="./assets/img/weather/Sun behind large cloud.png"
-                    alt=""
-                  />
-                </div>
-                <div class="temp">
-                  <span class="high">4</span>
-                  <span class="low">14</span>
-                </div>
-              </li>
-              <li>
-                <h3>FRI</h3>
-                <div class="icon">
-                  <img src="./assets/img/weather/Cloud.png" alt="" />
-                </div>
-                <div class="temp">
-                  <span class="high">3</span>
-                  <span class="low">15</span>
-                </div>
-              </li>
-              <li>
-                <h3>SAT</h3>
-                <div class="icon">
-                  <img
-                    src="./assets/img/weather/Cloud with lightning and rain.png"
-                    alt=""
-                  />
-                </div>
-                <div class="temp">
-                  <span class="high">6</span>
-                  <span class="low">14</span>
-                </div>
-              </li>
-              <li>
-                <h3>SUN</h3>
-                <div class="icon">
-                  <img
-                    src="./assets/img/weather/Sun behind small cloud.png"
-                    alt=""
-                  />
-                </div>
-                <div class="temp">
-                  <span class="high">8</span>
-                  <span class="low">16</span>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div class="exercise card">
-            <p>오늘 운동할 계획이 있으신가요?</p>
-            <span class="yes">
-              <img src="./assets/img/exercise/OK hand.png" alt="" />
-            </span>
-            <span class="no">
-              <img src="./assets/img/exercise/Waving hand.png" alt="" />
-            </span>
-          </div>
-          <div class="saying card">
-            <p>인생에서 가장 의미없이 보낸 날은 웃지 않고 보낸 날이다.</p>
-            <span class="man">E. E. 커밍스</span>
-          </div>
-          <div class="animal card">
-            <p>
-              키우시는 반려동물을 선택해주세요 !<br />
-              없다면 화분을 눌러주세요 !
-            </p>
-            <input class="gmark" type="text" placeholder="이름이 무엇인가요?" />
-            <span class="dog">
-              <img src={dog} alt="" />
-            </span>
-            <span class="plants">
-              <img src="./assets/img/animal/Potted plant.png" alt="" />
-            </span>
-            <span class="cat">
-              <img src={cat} alt="" />
-            </span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="weather card">
-            <div class="date">22/11/16수</div>
-            <div class="wea">
-              <img src={sbc} alt="" />
-            </div>
-            <tem class="tem">22℃</tem>
-            <div class="more">
-              <div class="wind">19km/h</div>
-              <div class="hum">22%</div>
-            </div>
-          </div>
-          <div class="clothes card">
-            <img src="./assets/img/clothes/후드티.png" alt="" />
-            <p>
-              약간은 쌀쌀할 수 있어요 !<br />
-              긴팔이나 가디건을 추천드립니다.
-            </p>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+  return <Today on={on} />;
 };
 
 export default MainConts;
