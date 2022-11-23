ff866efba0b9c2351ab411b0f8525271c9b75ea2
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-23 11:38:56 +0900

    2022.11.23

---
+++ src/components/MainConts.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+// import todayicon from "./assets/img/weather/Sun behind large cloud.png";
+const MainConts = () => {
+  return (
+    <main id="main">
+      <div class="wrap gmark">
+        <div class="left">
+          <div class="title">
+            <h1>OWATD</h1>
+            <p>
+              바쁜 일상 속 당신의 하루를
+              <br />
+              미리 준비해드립니다 :
+            </p>
+          </div>
+          <div class="week card">
+            <ul>
+              <li>
+                <h3>TODAY</h3>
+                <div class="icon">
+                  <img src="#" alt="todayicon" />
+                </div>
+                <div class="temp">
+                  <span class="high">5</span>
+                  <span class="low">12</span>
+                </div>
+              </li>
+              <li>
+                <h3>THU</h3>
+                <div class="icon">
+                  <img
+                    src="./assets/img/weather/Sun behind large cloud.png"
+                    alt=""
+                  />
+                </div>
+                <div class="temp">
+                  <span class="high">4</span>
+                  <span class="low">14</span>
+                </div>
+              </li>
+              <li>
+                <h3>FRI</h3>
+                <div class="icon">
+                  <img src="./assets/img/weather/Cloud.png" alt="" />
+                </div>
+                <div class="temp">
+                  <span class="high">3</span>
+                  <span class="low">15</span>
+                </div>
+              </li>
+              <li>
+                <h3>SAT</h3>
+                <div class="icon">
+                  <img
+                    src="./assets/img/weather/Cloud with lightning and rain.png"
+                    alt=""
+                  />
+                </div>
+                <div class="temp">
+                  <span class="high">6</span>
+                  <span class="low">14</span>
+                </div>
+              </li>
+              <li>
+                <h3>SUN</h3>
+                <div class="icon">
+                  <img
+                    src="./assets/img/weather/Sun behind small cloud.png"
+                    alt=""
+                  />
+                </div>
+                <div class="temp">
+                  <span class="high">8</span>
+                  <span class="low">16</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="exercise card">
+            <p>오늘 운동할 계획이 있으신가요?</p>
+            <span class="yes">
+              <img src="./assets/img/exercise/OK hand.png" alt="" />
+            </span>
+            <span class="no">
+              <img src="./assets/img/exercise/Waving hand.png" alt="" />
+            </span>
+          </div>
+          <div class="saying card">
+            <p>인생에서 가장 의미없이 보낸 날은 웃지 않고 보낸 날이다.</p>
+            <span class="man">E. E. 커밍스</span>
+          </div>
+          <div class="animal card">
+            <p>
+              키우시는 반려동물을 선택해주세요 !<br />
+              없다면 화분을 눌러주세요 !
+            </p>
+            <input class="gmark" type="text" placeholder="이름이 무엇인가요?" />
+            <span class="dog">
+              <img src="./assets/img/animal/Dog.png" alt="" />
+            </span>
+            <span class="plants">
+              <img src="./assets/img/animal/Potted plant.png" alt="" />
+            </span>
+            <span class="cat">
+              <img src="./assets/img/animal/Black cat.png" alt="" />
+            </span>
+          </div>
+        </div>
+        <div class="right">
+          <div class="weather card">
+            <div class="date">22/11/16수</div>
+            <div class="wea">
+              <img src="./assets/img/weather/Sun behind cloud.png" alt="" />
+            </div>
+            <div class="tem">22℃</div>
+            <div class="more">
+              <div class="wind">19km/h</div>
+              <div class="hum">22%</div>
+            </div>
+          </div>
+          <div class="clothes card">
+            <img src="./assets/img/clothes/후드티.png" alt="" />
+            <p>
+              약간은 쌀쌀할 수 있어요 !<br />
+              긴팔이나 가디건을 추천드립니다.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainConts;
---
+++ src/utils/content.js
@@ -0,0 +1,35 @@
+import { Gi3DGlasses } from 'react-icons/gi'
+import { Gi3DHammer } from 'react-icons/gi'
+import { Gi3DMeeple } from 'react-icons/gi'
+import { Gi3DStairs } from 'react-icons/gi'
+import { GiAbacus } from 'react-icons/gi'
+import { GiAbbotMeeple } from 'react-icons/gi'
+import { GiAbdominalArmor } from 'react-icons/gi'
+import { GiAbstract001 } from 'react-icons/gi'
+import { GiAbstract002 } from 'react-icons/gi'
+import { GiAbstract003 } from 'react-icons/gi'
+import { GiAbstract004 } from 'react-icons/gi'
+import { GiAbstract005 } from 'react-icons/gi'
+import { GiAbstract006 } from 'react-icons/gi'
+import { GiAbstract007 } from 'react-icons/gi'
+import { GiAbstract008 } from 'react-icons/gi'
+import { GiAbstract009 } from 'react-icons/gi'
+
+export const categories = [
+  { name: '웹스토리보디', icon: <Gi3DGlasses /> },
+  { name: '강민', icon: <Gi3DHammer /> },
+  { name: '발스타홍구', icon: <Gi3DMeeple /> },
+  { name: '괴물쥐', icon: <Gi3DStairs /> },
+  { name: 'PAKA', icon: <GiAbacus /> },
+  { name: '카사노박', icon: <GiAbbotMeeple /> },
+  { name: '듀리', icon: <GiAbdominalArmor /> },
+  { name: '코딩아빠', icon: <GiAbstract001 /> },
+  { name: '침착맨', icon: <GiAbstract002 /> },
+  { name: '코딩알려주는누나', icon: <GiAbstract003 /> },
+  { name: '셜록현준', icon: <GiAbstract004 /> },
+  { name: '겜스트', icon: <GiAbstract005 /> },
+  { name: '김태', icon: <GiAbstract006 /> },
+  { name: '게임디베이터스', icon: <GiAbstract007 /> },
+  { name: '강태풍', icon: <GiAbstract008 /> },
+  { name: '테스터훈', icon: <GiAbstract009 /> },
+]
