f2edaca10617e378ad1a2104b11646d4a04db351
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-12-01 10:33:36 +0900

    asd
    asd

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -7,22 +7,22 @@ import { getUltraSrtFcst } from "../utils/UltraSrtFcst";
 
 import Loader from "./Loader";
 
-import sblc from "../assets/img/weather/Sun behind large cloud.png";
-import cwlr from "../assets/img/weather/Cloud with lightning and rain.png";
-import sbc from "../assets/img/weather/Sun behind cloud.png";
-import dog from "../assets/img/animal/Dog.png";
-import bcat from "../assets/img/animal/Black cat.png";
-import wh from "../assets/img/exercise/Waving hand.png";
-import oh from "../assets/img/exercise/OK hand.png";
-import hh from "../assets/img/exercise/Heart hands.png";
-import ex1 from "../assets/img/exercise/Badminton.png";
-import cld from "../assets/img/weather/Cloud.png";
-import sbsc from "../assets/img/weather/Sun behind small cloud.png";
-import pp from "../assets/img/animal/Potted plant.png";
-import tree from "../assets/img/animal/tree.png";
-import blossom from "../assets/img/animal/Blossom.png";
-import dog2 from "../assets/img/animal/Guide dog.png";
-import ht from "../assets/img/clothes/hoodie.png";
+import Sblc from "../assets/img/weather/Sun behind large cloud.png";
+import Cwlr from "../assets/img/weather/Cloud with lightning and rain.png";
+import Sbc from "../assets/img/weather/Sun behind cloud.png";
+import Dog from "../assets/img/animal/Dog.png";
+import Bcat from "../assets/img/animal/Black cat.png";
+import Wh from "../assets/img/exercise/Waving hand.png";
+import Oh from "../assets/img/exercise/OK hand.png";
+import Hh from "../assets/img/exercise/Heart hands.png";
+import Ex1 from "../assets/img/exercise/Badminton.png";
+import Cld from "../assets/img/weather/Cloud.png";
+import Sbsc from "../assets/img/weather/Sun behind small cloud.png";
+import Pp from "../assets/img/animal/Potted plant.png";
+import Tree from "../assets/img/animal/tree.png";
+import Blossom from "../assets/img/animal/Blossom.png";
+import Dog2 from "../assets/img/animal/Guide dog.png";
+import Ht from "../assets/img/clothes/hoodie.png";
 
 import { say } from "../utils/say";
 
@@ -62,6 +62,7 @@ const Today = () => {
     getUltraSrtFcst().then((UltraSrtFcst) => setUltra(UltraSrtFcst));
   }, []);
   //console.log(ultra);
+  //초단기예보
 
   let t1h = [];
   for (let i = 0; i < ultra.length; i++) {
@@ -70,6 +71,7 @@ const Today = () => {
     }
   }
   //console.log(t1h);
+  //현재기온
 
   let sky = [];
   for (let i = 0; i < ultra.length; i++) {
@@ -77,7 +79,8 @@ const Today = () => {
       sky.push(ultra[i]);
     }
   }
-  //console.log(sky);
+  console.log(sky);
+  //하늘
 
   let pty = [];
   for (let i = 0; i < ultra.length; i++) {
@@ -85,7 +88,8 @@ const Today = () => {
       pty.push(ultra[i]);
     }
   }
-  //console.log(pty);
+  console.log(pty);
+  //대기
 
   let reh = [];
   for (let i = 0; i < ultra.length; i++) {
@@ -106,11 +110,10 @@ const Today = () => {
   //풍속
 
   const [fcst, setVilage] = useState([]);
-
   useEffect(() => {
     getVilageFcst().then((VilageFcst) => setVilage(VilageFcst));
   }, []);
-  //단기예보 어제날짜23시
+  //단기예보 어제 23시기준
   //console.log(fcst);
 
   let tmn = [];
@@ -119,7 +122,7 @@ const Today = () => {
       tmn.push(fcst[i]);
     }
   }
-  console.log(tmn);
+  //console.log(tmn);
   //최저기온
 
   let tmx = [];
@@ -128,15 +131,13 @@ const Today = () => {
       tmx.push(fcst[i]);
     }
   }
-  console.log(tmx);
+  //console.log(tmx);
   //최고기온
 
   var today = new Date();
-
   var year = today.getFullYear();
   var month = ("0" + (today.getMonth() + 1)).slice(-2);
   var day = ("0" + today.getDate()).slice(-2);
-
   var todaystring = year + month + day;
   //오늘날짜
 
@@ -152,13 +153,9 @@ const Today = () => {
   weekday[8] = "Mon";
   weekday[9] = "Tue";
   weekday[10] = "Wed";
-
   let gd = today.getDay();
-  // console.log(weekday[gd]);
-
-  function doAction() {
-    alert("이름을 알려주세요");
-  }
+  //console.log(weekday[gd]);
+  //요일로 바꾸기
 
   const [mid, setMid] = useState([]);
   useEffect(() => {
@@ -166,6 +163,11 @@ const Today = () => {
   }, []);
   //중기예보
   //console.log(mid);
+
+  function doAction() {
+    alert("이름을 알려주세요");
+  }
+
   if (!ultra?.length) return <Loader />;
   if (!fcst?.length && !mid?.length) return <Loader />;
   //로딩
@@ -173,7 +175,7 @@ const Today = () => {
     <main id="main">
       <meta
         http-equiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
+        content="default-src 'self'; script-src 'self'; style-src 'self'; "
       ></meta>
       <div className="wrap gmark">
         <div className="left">
@@ -190,56 +192,56 @@ const Today = () => {
               <li>
                 <h3>TODAY</h3>
                 <div className="icon">
-                  <img src={sbc} alt="todayicon" />
+                  <img src={Sbc} alt="todayicon" />
                 </div>
 
-                <div class="temp">
-                  <span class="high">{tmn[0].fcstValue.slice(0, -2)}</span>
-                  <span class="low">{tmx[0].fcstValue.slice(0, -2)}</span>
+                <div className="temp">
+                  <span className="high">{tmn[0].fcstValue.slice(0, -2)}</span>
+                  <span className="low">{tmx[0].fcstValue.slice(0, -2)}</span>
                 </div>
               </li>
               <li>
                 <h3>{weekday[gd + 1]}</h3>
                 <div className="icon">
-                  <img src={sblc} alt="" />
+                  <img src={Sblc} alt="" />
                 </div>
 
-                <div class="temp">
-                  <span class="high">{tmn[1].fcstValue.slice(0, -2)}</span>
-                  <span class="low">{tmx[1].fcstValue.slice(0, -2)}</span>
+                <div className="temp">
+                  <span className="high">{tmn[1].fcstValue.slice(0, -2)}</span>
+                  <span className="low">{tmx[1].fcstValue.slice(0, -2)}</span>
                 </div>
               </li>
               <li>
                 <h3>{weekday[gd + 2]}</h3>
                 <div className="icon">
-                  <img src={cld} alt="" />
+                  <img src={Cld} alt="" />
                 </div>
 
-                <div class="temp">
-                  <span class="high">{tmn[2].fcstValue.slice(0, -2)}</span>
-                  <span class="low">{tmx[2].fcstValue.slice(0, -2)}</span>
+                <div className="temp">
+                  <span className="high">{tmn[2].fcstValue.slice(0, -2)}</span>
+                  <span className="low">{tmx[2].fcstValue.slice(0, -2)}</span>
                 </div>
               </li>
               <li>
                 <h3>{weekday[gd + 3]}</h3>
                 <div className="icon">
-                  <img src={cwlr} alt="" />
+                  <img src={Cwlr} alt="" />
                 </div>
 
-                <div class="temp">
-                  <span class="high">{mid.taMin3}</span>
-                  <span class="low">{mid.taMax3}</span>
+                <div className="temp">
+                  <span className="high">{mid.taMin3}</span>
+                  <span className="low">{mid.taMax3}</span>
                 </div>
               </li>
               <li>
                 <h3>{weekday[gd + 4]}</h3>
                 <div className="icon">
-                  <img src={sbsc} alt="" />
+                  <img src={Sbsc} alt="" />
                 </div>
 
-                <div class="temp">
-                  <span class="high">{mid.taMin4}</span>
-                  <span class="low">{mid.taMax4}</span>
+                <div className="temp">
+                  <span className="high">{mid.taMin4}</span>
+                  <span className="low">{mid.taMax4}</span>
                 </div>
               </li>
             </ul>
@@ -250,21 +252,21 @@ const Today = () => {
               <div className="front">
                 <p>오늘 운동할 계획이 있으신가요?</p>
                 <span className="ok">
-                  <img src={oh} alt="" />
+                  <img src={Oh} alt="" />
                 </span>
                 <span className="waving">
-                  <img src={wh} alt="" />
+                  <img src={Wh} alt="" />
                 </span>
               </div>
               <div className="back no">
-                <img src={hh} alt="" />
+                <img src={Hh} alt="" />
                 <p>
                   건강한 삶에서 휴식은 필수 !<br /> 중간중간 스트레칭도 잊지
                   말아주세요 :D
                 </p>
               </div>
               <div className="back yes">
-                <img src={ex1} alt="" />
+                <img src={Ex1} alt="" />
                 <p>
                   햇빛이 강하지 않은 하루에요 !<br /> 집 앞 공원에서 배드민턴
                   어때요 ?
@@ -289,27 +291,27 @@ const Today = () => {
               />
               <span className="dog">
                 <button type="submit" onClick={doAction} className="tipBtn">
-                  <img src={dog} alt="강아지" />
+                  <img src={Dog} alt="강아지" />
                 </button>
               </span>
               <span className="plants">
                 <button type="submit" onClick={doAction} className="tipBtn">
-                  <img src={pp} alt="화분" />
+                  <img src={Pp} alt="화분" />
                 </button>
               </span>
               <span className="cat">
                 <button type="submit" onClick={doAction} className="tipBtn">
-                  <img src={bcat} alt="고양이" />
+                  <img src={Bcat} alt="고양이" />
                 </button>
               </span>
             </div>
             <div className="back dog">
               <div className="bg">
-                <img className="i1" src={tree} alt="나무" />
-                <img className="i2" src={blossom} alt="꽃" />
-                <img className="i3" src={tree} alt="나무" />
-                <img className="i4" src={dog2} alt="산책하는 강아지" />
-                <img className="i5" src={tree} alt="나무" />
+                <img className="i1" src={Tree} alt="나무" />
+                <img className="i2" src={Blossom} alt="꽃" />
+                <img className="i3" src={Tree} alt="나무" />
+                <img className="i4" src={Dog2} alt="산책하는 강아지" />
+                <img className="i5" src={Tree} alt="나무" />
               </div>
               <p>
                 ‘ㅇㅇ’와 산책하기 좋은 날이에요 :D <br />
@@ -319,22 +321,22 @@ const Today = () => {
           </div>
         </div>
 
-        <div class="right">
-          <div class="weather card">
-            <div class="date">
+        <div className="right">
+          <div className="weather card">
+            <div className="date">
               {todaystring} &lt;{weekday[gd]}&gt;
             </div>
-            <div class="wea">
-              <img src={sbc} alt="" />
+            <div className="wea">
+              <img src={Sbc} alt="" />
             </div>
-            <tem class="tem">{t1h[0].fcstValue}℃</tem>
-            <div class="more">
-              <div class="wind">{wsd[0].fcstValue}m/s</div>
-              <div class="hum">{reh[0].fcstValue}%</div>
+            <div className="tem">{t1h[0].fcstValue}℃</div>
+            <div className="more">
+              <div className="wind">{wsd[0].fcstValue}m/s</div>
+              <div className="hum">{reh[0].fcstValue}%</div>
             </div>
           </div>
           <div className="clothes card">
-            <img src={ht} alt="" />
+            <img src={Ht} alt="" />
             <p>
               약간은 쌀쌀할 수 있어요 !<br />
               긴팔이나 가디건을 추천드립니다.
